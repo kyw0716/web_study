8a67d74796c4d12e5398fcd18b4a1ffb8965f683
김영우 <kyw0716@naver.com>
2021-01-15 02:47:05 +0900

    google clone coding ver.2

---
+++ html 연습2/index_UI.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('.menu_overflow_btn').on('click', function(){
+        $('.menu_overflow').toggleClass('active');
+    });
+});
+
+$(function(){
+    $('.menu_btn').on('click',function(){
+        $('.login_page_overflow').removeClass('active');
+    })
+})
+
+$(function(){
+    $('.login_page_next_box').on('click',function(){
+        $('.login_page_overflow').addClass('active');
+    })
+})
